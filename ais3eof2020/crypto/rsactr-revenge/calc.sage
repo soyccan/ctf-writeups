8b650fb2d9b0d0f3e4eba171716d74f6a1c71e0c
soyccan <soyccan@gmail.com>
2020-01-30 02:00:58 +0800

    done rsactr-revenge

--- ais3eof2020/crypto/rsactr-revenge/calc.sage
+++ ais3eof2020/crypto/rsactr-revenge/calc.sage
@@ -1,9 +1,7 @@
 import sys
+sys.setrecursionlimit(10000)
 
 def gcd(f1, f2):
-    print('f1',f1)
-    print('f2',f2)
-    print()
     return f1.monic() if f2 == 0 else gcd(f2, f1 % f2)
 
 c1 = bytes.fromhex(sys.argv[1])
@@ -11,6 +9,8 @@ c2 = bytes.fromhex(sys.argv[2])
 c3 = bytes.fromhex(sys.argv[3])
 e = int(sys.argv[4])
 n = int(sys.argv[5])
+
+print(f'e={e} c1={c1.hex()}')
 
 P.<x> = PolynomialRing(Zmod(n))
 
@@ -23,8 +23,8 @@ for i in range(0, len(c1), 128):
     f1 = (cc1 - x)^e - (cc2 - x)^e + 2020 * (len(c1) // 128)
     f2 = (cc2 - x)^e - (cc3 - x)^e + 2020 * (len(c1) // 128)
 
-    xx = -gcd(f1, f2).monic().coefficients[0]
-    flag += int(xx).to_bytes(16, 'little')
+    xx = -gcd(f1, f2).coefficients()[0]
+    flag += int(xx).to_bytes(16, 'big')
 print(flag)
 
 #
--- ais3eof2020/crypto/rsactr-revenge/solve.py
+++ ais3eof2020/crypto/rsactr-revenge/solve.py
@@ -4,26 +4,25 @@ import subprocess
 
 context.log_level = 'debug'
 
-r = remote('localhost', 1111)
+r = remote('localhost', 3333)
 
 r.sendlineafter('> ', '1')
 n = int(r.recvline()[4:])
 e = int(r.recvline()[4:])
 
 r.sendlineafter('> ', '2')
-c1 = bytes.fromhex(r.recvline()[7:-1].decode())
+c1 = bytes.fromhex(r.recvline().strip()[7:].decode())
 r.sendlineafter('> ', '2')
-c2 = bytes.fromhex(r.recvline()[7:-1].decode())
+c2 = bytes.fromhex(r.recvline().strip()[7:].decode())
 r.sendlineafter('> ', '2')
-c3 = bytes.fromhex(r.recvline()[7:-1].decode())
+c3 = bytes.fromhex(r.recvline().strip()[7:].decode())
 
-proc = subprocess.run([
+proc = subprocess.Popen([
     '/opt/SageMath/sage',
-    'coppersmith.sage',
+    'calc.sage',
     c1.hex(),
     c2.hex(),
     c3.hex(),
     str(e),
-    str(n)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-print(proc.stdout.decode())
-print(proc.stderr.decode())
+    str(n)])
+proc.wait()
