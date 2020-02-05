3c7b6a05cb495212fa81d8cfe9f58918e3d7f37e
chengkongtw <chengkongtw@gmail.com>
2020-02-05 09:42:33 +0800

    tmp

--- ais3eof2020/pre/misc/unlucky/test.py
+++ ais3eof2020/pre/misc/unlucky/test.py
@@ -1,4 +1,7 @@
 import subprocess
 f = open('/tmp/ebech2948', 'w')
+f.write('abc')
+f.flush()
 print(f.fileno())
+print(open('/proc/self/fdinfo/3', 'r').read())
 p = subprocess.run(input(': ').split())
