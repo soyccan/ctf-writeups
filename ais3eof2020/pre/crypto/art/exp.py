7d59b9a0cb3991d7e4b280c78b7c23dbab928249
soyccan <soyccan@gmail.com>
2020-02-06 18:48:58 +0800

    partially done art

--- ais3eof2020/pre/crypto/art/exp.py
+++ ais3eof2020/pre/crypto/art/exp.py
@@ -1,45 +1,21 @@
-import owiener
 from pwn import *
-from Crypto.Util.number import *
+import sys
+import owiener
+import subprocess
 
-nbit = 384
+def iter_factor(fact, res, n, idx):
+    if idx >= len(fact):
+        res.append(n)
+        return
 
-#  r = remote('ais3eof.zoolab.org', 9000)
-#  n = int(r.recvline()[4:])
-#  e = int(r.recvline()[4:])
+    base, exp = fact[idx]
+    for e in range(0, exp+1):
+        iter_factor(fact, res, n, idx+1)
+        n *= base
 
-#  d = owiener.attack(e, n)
+r = remote('ais3eof.zoolab.org', 9000, timeout=1)
 
-#  print(d)
-
-def gcd(a, b):
-    if b == 0:
-        return (a, 0, 1)
-    g, x, y = gcd(b, a % b)
-    return (g, y - (a // b) * x, x)
-
-def modinv(a, m):
-    g, x, y = gcd(m, a)
-    if g != 1:
-        return -1
-    return x % m
-
-def gen():
-    x, y, z, u = [getPrime(nbit) for _ in range(4)]
-    phi = (x-1)*(y-1)*(z-1)*(u-1)
-    n = x*y*z*u
-    while True:
-        d = getRandomInteger(192)
-        e = modinv(d, phi)
-        if e == -1:
-            continue
-        break
-    return n, e, d, phi, [x, y, z, u]
-
-n, e, d, phi, xs = gen()
-
-print('n='+str(n))
-print('e='+str(e))
-print('d='+str(d))
-print('phi='+str(phi))
-assert e*d%phi == 1
+n = int(r.recvline()[4:])
+e = int(r.recvline()[4:])
+phi = owiener.attack(e, n)
+success('phi='+str(phi))
