dd16077c9fb0f986482cc10cf149c94581be9c7b
chengkongtw <chengkongtw@gmail.com>
2020-02-07 03:32:45 +0800

    .gitignore

--- ais3eof2020/on-site/crypto/art/exp.py
+++ ais3eof2020/on-site/crypto/art/exp.py
@@ -1,17 +1,5 @@
 from pwn import *
-import sys
 import owiener
-import subprocess
-
-def iter_factor(fact, res, n, idx):
-    if idx >= len(fact):
-        res.append(n)
-        return
-
-    base, exp = fact[idx]
-    for e in range(0, exp+1):
-        iter_factor(fact, res, n, idx+1)
-        n *= base
 
 r = remote('ais3eof.zoolab.org', 9000, timeout=1)
 
